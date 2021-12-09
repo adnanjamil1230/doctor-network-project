2cb511e860a092a9fef50e1f4c643b5fef583e6a
adnanjamil1234 <adnanjamil.mediapark@gmail.com>
2021-12-09 16:23:00 +0500

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import * as $ from "jquery";
+import { AwareComponent } from './aware/aware.component';
+import { ArticleComponent } from './article/article.component';
+
+import { CounterComponent } from './counter/counter.component';
+import { WorkComponent } from './work/work.component';
+import { TestComponent } from './test/test.component';
+import { CtaComponent } from './cta/cta.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    AwareComponent,
+    ArticleComponent,
+    CounterComponent,
+    WorkComponent,
+    TestComponent,
+    CtaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/aware/aware.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AwareComponent } from './aware.component';
+
+describe('AwareComponent', () => {
+  let component: AwareComponent;
+  let fixture: ComponentFixture<AwareComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AwareComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AwareComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
